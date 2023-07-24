26f5a558e7260225fa3bb292448a01c6ee1acfd2
lee651 <2864172651@qq.com>
2023-07-24 23:53:18 +0800

    修改swagger接口文档

--- src/main/java/top/rectorlee/config/SwaggerConfig.java
+++ src/main/java/top/rectorlee/config/SwaggerConfig.java
@@ -1,8 +1,9 @@
 package top.rectorlee.config;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +19,11 @@ public class SwaggerConfig {
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(new ApiInfoBuilder().title("微信支付接口").build());
+                .apiInfo(new ApiInfoBuilder().title("支付接口").build())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
     }
 }
