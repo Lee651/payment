7b023d54bd2a7195e6df6a25b3d15bfd38aeee25
JackeyLee <2864172651@qq.com>
2023-08-28 14:05:34 +0800

    添加jasypt依赖, 对配置文件相关信息进行加密

--- src/test/java/top/rectorlee/AllTest.java
+++ src/test/java/top/rectorlee/AllTest.java
@@ -1,6 +1,7 @@
 package top.rectorlee;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,21 +15,22 @@ import org.springframework.core.env.Environment;
 @Slf4j
 @SpringBootTest
 public class AllTest {
-    /*@Autowired
-    private WxPayConfig wxPayConfig;
-
-    @Test
-    public void testWxPayInfo() {
-        String privateKeyPath = wxPayConfig.getPrivateKeyPath();
-        PrivateKey privatekey = wxPayConfig.getPrivatekey(privateKeyPath);
-        System.out.println(privatekey);
-    }*/
-
     @Autowired
     private Environment environment;
+
+    @Autowired
+    private StringEncryptor stringEncryptor;
 
     @Test
     public void testAliPay() {
         log.warn("alipay参数为: {}", environment.getProperty("alipay.app-id"));
+    }
+
+    @Test
+    public void testJasypt() {
+        String root = stringEncryptor.encrypt("jdbc:mysql://localhost:3306/payment?characterEncoding=UTF-8&useSSL=false&serverTimezone=Asia/Shanghai");
+        String decrypt = stringEncryptor.decrypt(root);
+        System.out.println("明文为: " + decrypt);
+        System.out.println("密文为: " + root);
     }
 }
